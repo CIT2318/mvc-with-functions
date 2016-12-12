d335daff5ad00b6c145be02e9577deda4a1df82e
mmantle-hud <m.e.mantle@hud.ac.uk>
2016-12-12 10:38:20 +0000

    Updated title

--- list.php
+++ list.php
@@ -1,15 +1,10 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>PHP, PDO, Databases</title>
+<title>Show All Films</title>
 <meta http-equiv="content-type" content="text/html;charset=utf-8" />
 </head>
 <body>
-<ul>
-	<li><a href="list.php">View all films</a></li>
-	<li><a href="insert-form.php">Add a new film</a></li>
-	<li><a href="delete-form.php">Delete films</a></li>
-</ul>
 <?php
 try{
        $conn = new PDO('mysql:host=localhost;dbname=u0123456', 'u0123456', '01jan96');
